bd77345abb864d8848cd98c597ea6fb917bcdf35
qwerty1434 <qwerty1434@naver.com>
2022-06-23 14:01:33 +0900

    프로젝트 생성

---
+++ src/main/java/com/qwerty1434/dart/DartApplication.java
@@ -0,0 +1,13 @@
+package com.qwerty1434.dart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qwerty1434/dart/DartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerty1434.dart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
