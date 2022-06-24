d2cf4d71ed628e10558d54fe136ca6693a31af6a
qwerty1434 <qwerty1434@naver.com>
2022-06-24 14:27:35 +0900

    mybatis연동 - 컨트롤러 및 자잘한 수정

--- src/main/java/com/qwerty1434/dart/controller/UserController.java
+++ src/main/java/com/qwerty1434/dart/controller/UserController.java
@@ -1,15 +1,32 @@
 package com.qwerty1434.dart.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qwerty1434.dart.dao.User;
+import com.qwerty1434.dart.dto.UserDto;
+
 @RestController
 @RequestMapping("/User")
 public class UserController {
-	@GetMapping("/SearchAll")
-	public String NextPage() {
-		System.out.println("We Must Go To Next Page");
-		return "index";
+	@Autowired
+	private User dao;
+	
+	@GetMapping("/searchAll")
+	public ResponseEntity<?> searchAll() throws SQLException{
+		List<UserDto> result = dao.selectAll();
+		if(result != null) {
+			return new ResponseEntity<List<UserDto>>(result,HttpStatus.OK);
+		}else {
+			return new ResponseEntity<String>("FAIL",HttpStatus.NO_CONTENT);
+		}
 	}
+
 }
--- src/main/resources/dart-front/src/views/IndexView.vue
+++ src/main/resources/dart-front/src/views/IndexView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>HelloWorld</h1>
-    hihihihi
+    localhost:80/User/searchAll을 호출하고, 그 결과로 받아온 ResponseEntity를
+    뿌려야 함
   </div>
 </template>
