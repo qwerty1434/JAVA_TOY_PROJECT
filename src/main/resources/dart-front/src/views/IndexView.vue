8997e9731efa541638804d0b1a8945cb83de1b6f
qwerty1434 <qwerty1434@naver.com>
2022-06-25 11:09:21 +0900

    데이터 주고받기 - Vue에서 데이터 뿌리기

--- src/main/resources/dart-front/src/views/IndexView.vue
+++ src/main/resources/dart-front/src/views/IndexView.vue
@@ -3,16 +3,23 @@
     <h1>HelloWorld</h1>
 
     <button @click="searchAll">데이터 모두조회</button>
+    {{ user }}
   </div>
 </template>
 <script>
 import url from "@/url/http";
 
 export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
   methods: {
     searchAll() {
       url.get("/User/searchAll", {}).then((response) => {
         console.log(response);
+        this.user = response.data;
       });
     },
   },
